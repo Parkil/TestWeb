208fd53df9a577241f7119fb5da33ffd8f53cb59
Parkil <alkain77@gmail.com>
2015-05-20 17:11:16 +0900

    First Upload
    최초 업로드
    - Convert 서비스 미등록

---
+++ src/main/java/egovframework/example/cmmn/service/FileVO.java
@@ -0,0 +1,60 @@
+package egovframework.example.cmmn.service;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/*
+ * MultipartFile -> FileVO Convert를 위한 VO
+ */
+public class FileVO {
+	private String orgFileName;
+	private String realFileName;
+	private String ext;
+	private File file;
+	private MultipartFile multipartfile;
+	private long fileSize;
+	
+	public String getOrgFileName() {
+		return orgFileName;
+	}
+	public void setOrgFileName(String orgFileName) {
+		this.orgFileName = orgFileName;
+	}
+	public String getRealFileName() {
+		return realFileName;
+	}
+	public void setRealFileName(String realFileName) {
+		this.realFileName = realFileName;
+	}
+	public String getExt() {
+		return ext;
+	}
+	public void setExt(String ext) {
+		this.ext = ext;
+	}
+	public File getFile() {
+		return file;
+	}
+	public void setFile(File file) {
+		this.file = file;
+	}
+	public MultipartFile getMultipartfile() {
+		return multipartfile;
+	}
+	public void setMultipartfile(MultipartFile multipartfile) {
+		this.multipartfile = multipartfile;
+	}
+	public long getFileSize() {
+		return fileSize;
+	}
+	public void setFileSize(long fileSize) {
+		this.fileSize = fileSize;
+	}
+	
+	public void transferFile(String filePath) throws IllegalAccessException, IOException {
+		file = new File(filePath);
+		multipartfile.transferTo(file);
+	}
+}
---
+++ src/main/java/egovframework/example/util/MultipartFileToFileVOConverter.java
@@ -0,0 +1,38 @@
+package egovframework.example.util;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.web.multipart.MultipartFile;
+
+import egovframework.example.cmmn.service.FileVO;
+
+/*
+ * MultipartFile을 FileVO로 변환하는 클래스
+ */
+public class MultipartFileToFileVOConverter implements Converter<MultipartFile, FileVO> {
+
+	@Override
+	public FileVO convert(MultipartFile source) {
+		System.out.println("Convertttttt");
+		FileVO result = null;
+		
+		if(source == null) {
+			result = null;
+		}else {
+			result = new FileVO();
+			String orgFileName = source.getOriginalFilename();
+			result.setOrgFileName(orgFileName);;
+			result.setMultipartfile(source);
+			result.setFileSize(source.getSize());
+			
+			int idx = orgFileName.lastIndexOf(".");
+			if(idx == -1) {
+				result.setExt(null);
+			}else {
+				String ext = orgFileName.substring(idx+1);
+				result.setExt(ext);
+			}
+		}
+		
+		return result;
+	}
+}
