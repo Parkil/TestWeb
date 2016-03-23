51a4f0dc891d510e30cd06fde78631078af00fb0
Parkil <alkain77@gmail.com>
2016-03-23 21:41:24 +0900

    1.IEDriver 업데이트 2.템플릿 자동생성 테스트 케이스 업데이트 3.Util Driver 코드 업데이트(DesiredCapabilities 관련 소스 수정)

--- src/test/java/util/Util.java
+++ src/test/java/util/Util.java
@@ -27,13 +27,24 @@ public class Util {
 		
 		System.setProperty("webdriver.ie.driver", sb.toString());
 		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setCapability("nativeEvents", false);    
+		dc.setCapability("unexpectedAlertBehaviour", "accept");
+		dc.setCapability("ignoreProtectedModeSettings", true);
+		dc.setCapability("disable-popup-blocking", true);
+		dc.setCapability("enablePersistentHover", true);
+
 		return new InternetExplorerDriver(dc);
 	}
 	
 	//firegox 용 웹 드라이버 클래스 반환
 	public static WebDriver getFireFoxDriver() {
-		System.setProperty("webdriver.firefox.bin", "d:/Util/FirefoxPortable/firefox.exe");
+		System.setProperty("webdriver.firefox.bin", "D:/Util/FirefoxPortable/FirefoxPortable.exe");
 		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setCapability("nativeEvents", false);    
+		dc.setCapability("unexpectedAlertBehaviour", "accept");
+		dc.setCapability("ignoreProtectedModeSettings", true);
+		dc.setCapability("disable-popup-blocking", true);
+		dc.setCapability("enablePersistentHover", true);
 		return new FirefoxDriver(dc);
 	}
 }
