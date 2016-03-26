c5eac0b1a134fef16fa8b32d69b10737419d3913
Parkil <alkain77@gmail.com>
2016-03-27 08:58:58 +0900

    1.Chrome Driver 로직 추가 2.Selenium template generator 테스트 코드 업로드

--- src/test/java/util/Util.java
+++ src/test/java/util/Util.java
@@ -3,6 +3,8 @@ package util;
 import java.io.File;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -38,13 +40,37 @@ public class Util {
 	
 	//firegox 용 웹 드라이버 클래스 반환
 	public static WebDriver getFireFoxDriver() {
-		System.setProperty("webdriver.firefox.bin", "D:/Util/FirefoxPortable/FirefoxPortable.exe");
+		System.setProperty("webdriver.firefox.bin", "D:/Util/FirefoxPortable/App/Firefox/firefox.exe");
+		
 		DesiredCapabilities dc = new DesiredCapabilities();
-		dc.setCapability("nativeEvents", false);    
+		/*dc.setCapability("nativeEvents", false);    
 		dc.setCapability("unexpectedAlertBehaviour", "accept");
 		dc.setCapability("ignoreProtectedModeSettings", true);
 		dc.setCapability("disable-popup-blocking", true);
 		dc.setCapability("enablePersistentHover", true);
+		*/
 		return new FirefoxDriver(dc);
+	}
+	
+	//chrome용 웹 드라이버 클래스 반환
+	public static WebDriver getChromeDriver() {
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append(System.getProperty("user.dir"));
+		sb.append(File.separator);
+		sb.append("src");
+		sb.append(File.separator);
+		sb.append("test");
+		sb.append(File.separator);
+		sb.append("resources");
+		sb.append(File.separator);
+		sb.append("chromedriver.exe");
+		
+		System.out.println(sb.toString());
+		
+		System.setProperty("webdriver.chrome.driver", sb.toString()); //driver위치 반환
+		ChromeOptions co = new ChromeOptions();
+		co.setBinary("D:/Util/GoogleChromePortable/App/Chrome-bin/chrome.exe"); //cannot find chrome binary 예외 발생시에는 chrome실행파일을 수동으로 지정해 주어야 함
+		return new ChromeDriver(co);
 	}
 }
